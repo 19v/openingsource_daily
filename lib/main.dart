62acb18d4dd4e89913eae323a14e19503073870a
Haochen <jhc_98219@outlook.com>
2019-04-11 01:01:21 +0800

    继续调整页面,下一步准备接入API

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '开源日报',
+      color: Color(0xff4384f7),
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColorBrightness: Brightness.light,
+        brightness: Brightness.light,
+        backgroundColor: Color(0xfff2f3f5),
       ),
       home: HomePage(),
     );
@@ -20,83 +24,57 @@ class MyApp extends StatelessWidget {
 }
 
 //首页，定义导航栏等基本框架。
-
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  // 底部导航栏具体实现
-  int _currentIndex = 1;
-  final List<Widget> _children = [ 
-    CalendarPage(),
-    DailyPostPage(),
-    SettingPage()
-  ];
-  final List<BottomNavigationBarItem> _list = <BottomNavigationBarItem>[
-    _navBarSet(Icons.calendar_today, "日历"),
-    _navBarSet(Icons.adb, "今日推送"),
-    _navBarSet(Icons.settings, "设置"),
-  ];
-  // 页面标题
-  String _createTitle() {
-    String _title;
-    switch(_currentIndex) {
-      case 0:
-        _title = "往期日报";
-        break;
-      case 1:
-        _title = "今日日报";
-        break;
-      case 2:
-        _title = "各项设置";
-        break;
-      default:
-        _title = "404";
-        break;
-    }
-    return _title;
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
-  // 页面生成
+
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 3, initialIndex: 1);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_createTitle()),
+        leading: null,
+        elevation: 0,
+        backgroundColor: Color(0xfff2f3f5),
+        title: DefaultTabController(
+          length: 3,
+          child: TabBar(
+            indicatorWeight: 1.0,
+            labelColor: Color(0xff4384f7),
+            unselectedLabelColor: Color(0xff666666),
+            indicatorSize: TabBarIndicatorSize.label,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.calendar_today)),
+              Tab(icon: Icon(Icons.adb)),
+              Tab(icon: Icon(Icons.settings)),
+            ],
+            controller: _tabController,
+          ),
+        ),
       ),
-      body: _children[_currentIndex],
-      //底部导航
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        fixedColor: Color(0xFFF2F3F5),
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        currentIndex: _currentIndex,
-        items: _list
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CalendarPage(),
+          DailyPostPage(),
+          SettingPage()
+        ],
       ),
     );
   }
-}
-
-//底部栏每个选项卡的实现
-BottomNavigationBarItem _navBarSet(icons, text){
-  return BottomNavigationBarItem(
-    icon: Icon(
-      icons,
-      color: Colors.black87,
-    ),
-    title: Text(
-      text, 
-      style: TextStyle(
-        fontFamily: 'FZXYJT',
-        fontWeight: FontWeight.bold,
-        color: Colors.grey,
-      )
-    )
-  );
 }
