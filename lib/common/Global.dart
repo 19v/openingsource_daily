ea150369a7c747eb15d48ca3ded4efa63cf80ec8
Haochen <jhc_98219@outlook.com>
2019-12-07 17:12:50 +0800

    更改接口为反代接口

--- lib/common/Global.dart
+++ lib/common/Global.dart
@@ -2,7 +2,8 @@
 
 //api接口相关
 class ApiUrl{
-  static String apiUrl = "https://openingsource.org/wp-json/wp/v2/";
+  //static String apiUrl = "https://openingsource.org/wp-json/wp/v2/";
+  static String apiUrl = "https://os.maxoyed.com/wp-json/wp/v2/";
   //切换api接口（0、默认、使用原接口，1、使用国内反代）
   static void changeApiUrl(int i) {
     switch(i) {
